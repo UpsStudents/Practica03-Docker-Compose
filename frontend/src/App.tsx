a48f45375a0990265a2dc1bda10bb5766d4e81fd
Nicolas Cisneros <bryannicolas67@gmail.com>
2022-11-17 20:47:26 -0500

    primera version funcional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { Contact } from "./Components/Contact";
+import { Formik, Field, FormikHelpers } from "formik";
+import Form from "react-bootstrap/Form";
+
+interface Contact {
+  name: string;
+  phoneNumber: string;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Formik
+        initialValues={{
+          name: "",
+          phoneNumber: "",
+        }}
+        onSubmit={(
+          values: Contact,
+          { setSubmitting }: FormikHelpers<Contact>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      ></Formik>
+
+      <Form style={{ margin: "1rem" }}>
+        <Form.Group className="mb-3">
+          <Form.Label>Nombre:</Form.Label>
+          <Form.Control type="text" placeholder="Ingresa un nombre" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Número telefónico:</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Ingresa un número telefónico"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Guardar
+        </Button>
+      </Form>
+      <div className="contacts-container">
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/Components/Contact/index.tsx
@@ -0,0 +1,12 @@
+import { Card } from "react-bootstrap";
+
+export const Contact = () => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>Contacto</Card.Title>
+        <Card.Text>0987037858</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
